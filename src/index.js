4e0ff1c01d219cbddb266764123926a36077f6f6
medsonmoombe <medsonmoombe21@gmail.com>
2022-09-21 12:35:59 +0200

    Add the reducer and store  components

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import store from './store/Store';
-import App from './components/App';
+
 
 
 ReactDOM.render(
